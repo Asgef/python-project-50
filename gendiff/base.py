18b070b32b6bc74f18739f5bd46365f065fce529
Ivan <asgefes1@gmail.com>
2023-12-07 17:23:57 +0300

    Fix base.py

--- gendiff/base.py
+++ gendiff/base.py
@@ -2,8 +2,9 @@ import json
 
 
 def generate_diff(path_file1, path_file2):
-    file1 = json.load(open(path_file1))
-    file2 = json.load(open(path_file2))
+    with open(path_file1) as file1, open(path_file2) as file2:
+        file1 = json.load(file1)
+        file2 = json.load(file2)
     data = select_data(file1, file2)
     analyze = analyze_data(data)
     return analyze
@@ -30,12 +31,21 @@ def analyze_data(data):
     for key in data['names']:
         if 'first_file' in data[key] and 'second_file' in data[key]:
             if data[key]['first_file'] == data[key]['second_file']:
-                res_diff.append(f'    {key}: {data[key]['first_file']}')
+                res_diff.append(create_data(' ', key, data[key]['first_file']))
             else:
-                res_diff.append(f'  - {key}: {data[key]['first_file']}')
-                res_diff.append(f'  + {key}: {data[key]['second_file']}')
+                res_diff.append(create_data('-', key, data[key]['first_file']))
+                res_diff.append(create_data('+', key, data[key]['second_file']))
         if 'first_file' in data[key] and 'second_file' not in data[key]:
-            res_diff.append(f'  - {key}: {data[key]['first_file']}')
+            res_diff.append(create_data('-', key, data[key]['first_file']))
         if 'first_file' not in data[key] and 'second_file' in data[key]:
-            res_diff.append(f'  + {key}: {data[key]['second_file']}')
+            res_diff.append(create_data('+', key, data[key]['second_file']))
     return '{\n' + '\n'.join(res_diff) + '\n}'
+
+
+def create_data(sing, name, value):
+    if isinstance(value, bool):
+        if value:
+            value = 'true'
+        else:
+            value = 'false'
+    return f'  {sing} {name}: {value}'
