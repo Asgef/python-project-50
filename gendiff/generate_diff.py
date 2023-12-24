c4c0e44ba1028c47f6631154aafa288674d5080e
Ivan <asgefes1@gmail.com>
2023-12-24 18:20:47 +0300

    Fix docstring

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -56,6 +56,20 @@ def select_format(data, form):
 
 
 def open_file(file_path: str) -> tuple[str, str]:
+    """
+    Open and download the file in YAML or JSON format.
+
+    This function takes a file path as arguments,
+    opens the file in YAML or JSON format, creates a file object,
+    and converts the data to a string.
+    The loaded data is returned as a tuple, text data
+    and a string with the name of the extension.
+
+    :param file_path: Path to the file to open and load.
+    :type file_path: str
+    :return: Loaded data and format.
+    :rtype: tuple
+    """
     with open(file_path, 'r') as file:
         text = file.read()
     if file_path.endswith('.json'):
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -3,6 +3,19 @@ import yaml
 
 
 def parse(data: str, data_format: str) -> dict:
+    """
+    Parsing data from JSON or YAML into a dictionary.
+
+    This function accepts data as a string and the name
+    of the format to which this data belongs.
+    Depending on the name of the format, JSON or YAML
+    data is converted into a dictionary. The data is returned.
+
+    :param data: Text data
+    :param data_format: Format
+    :return: Parsed data
+    :rtype: dict
+    """
     if data_format == 'json':
         return json.loads(data)
     elif data_format == 'yaml':
